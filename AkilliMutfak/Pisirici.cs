caabde06ccb0265e8aa6f7d877a2d2084ee2b948
SCY95 <canyilmaz999@gmail.com>
2018-04-13 00:31:20 +0300

    FactoryBitti(Gelistirilebilir)

---
+++ AkilliMutfak/Pisirici.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AkilliMutfak
+{
+    public abstract class Pisirici
+    {
+        public Yemek YemekIste(string yemekTuru)
+        {
+            Yemek yemek = YemekYap(yemekTuru);
+
+            yemek.Hazirla();
+            yemek.Pisir();
+            yemek.Bol();
+            yemek.Servis();
+
+            return yemek;
+        }
+
+        protected abstract Yemek YemekYap(string yemekTuru);
+    }
+
+    public class Firin : Pisirici
+    {
+        protected override Yemek YemekYap(string yemekTuru)
+        {
+            Yemek yemek = null;
+
+            if (yemekTuru == "Sebze")
+                yemek = new FirindaSebzeliYemek();
+            else if (yemekTuru == "Balik")
+                yemek = new FirindaBalik();
+            else if (yemekTuru == "Corba")
+                yemek = new FirindaDomatesCorbasi();
+
+            return yemek;
+        }
+    }
+
+    public class Ocak : Pisirici
+    {
+        protected override Yemek YemekYap(string yemekTuru)
+        {
+            Yemek yemek = null;
+
+            if (yemekTuru == "Sebze")
+                yemek = new OcaktaSebzeliYemek();
+            else if (yemekTuru == "Balik")
+                yemek = new OcaktaBalik();
+            else if (yemekTuru == "Corba")
+                yemek = new OcaktaCorba();
+
+            return yemek;
+        }
+    }
+
+}
---
+++ AkilliMutfak/Yemek.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AkilliMutfak
+{
+    public abstract class Yemek
+    {
+        public virtual void Hazirla()
+        {
+            Console.WriteLine("Malzeme hazirla");
+        }
+
+        public virtual void Pisir()
+        {
+            Console.WriteLine("Pisir");
+        }
+
+        public virtual void Bol()
+        {
+            Console.WriteLine("Bol");
+        }
+
+        public virtual void Servis()
+        {
+            Console.WriteLine("Servis et");
+        }
+    }
+
+
+
+    class FirindaSebzeliYemek : Yemek
+    {
+        public virtual void Hazirla()
+        {
+            Console.WriteLine("Sebzeleri hazirla");
+        }
+
+        public virtual void Pisir()
+        {
+            Console.WriteLine("Firinda pisir");
+        }
+
+        public virtual void Bol()
+        {
+            Console.WriteLine("Tabaklara koy");
+        }
+
+        public virtual void Servis()
+        {
+            Console.WriteLine("Servis et");
+        }
+    }
+
+
+    class FirindaBalik : Yemek
+    {
+        public virtual void Hazirla()
+        {
+            Console.WriteLine("Baliklari ve cesnileri hazirla");
+        }
+
+        public virtual void Pisir()
+        {
+            Console.WriteLine("Firinda pisir");
+        }
+
+        public virtual void Bol()
+        {
+            Console.WriteLine("Tabaklara koy");
+        }
+
+        public virtual void Servis()
+        {
+            Console.WriteLine("Servis et");
+        }
+    }
+
+    
+    class FirindaDomatesCorbasi : Yemek
+    {
+        public virtual void Hazirla()
+        {
+            Console.WriteLine("Domatesleri hazirla");
+        }
+
+        public virtual void Pisir()
+        {
+            Console.WriteLine("Firinda Pisir");
+        }
+
+        public virtual void Bol()
+        {
+            Console.WriteLine("Kaselere koy");
+        }
+
+        public virtual void Servis()
+        {
+            Console.WriteLine("Servis et");
+        }
+    }
+
+
+
+    //----------------------------------------------------------------------------------------------------------------
+
+
+    class OcaktaSebzeliYemek : Yemek
+    {
+        public virtual void Hazirla()
+        {
+            Console.WriteLine("Sebzeleri hazirla");
+        }
+
+        public virtual void Pisir()
+        {
+            Console.WriteLine("Ocakta pisir");
+        }
+
+        public virtual void Bol()
+        {
+            Console.WriteLine("Tabaklara koy");
+        }
+
+        public virtual void Servis()
+        {
+            Console.WriteLine("Servis et");
+        }
+    }
+
+
+    class OcaktaBalik : Yemek
+    {
+        public virtual void Hazirla()
+        {
+            Console.WriteLine("Baliklari ve cesnileri hazirla");
+        }
+
+        public virtual void Pisir()
+        {
+            Console.WriteLine("Ocakta pisir");
+        }
+
+        public virtual void Bol()
+        {
+            Console.WriteLine("Tabaklara koy");
+        }
+
+        public virtual void Servis()
+        {
+            Console.WriteLine("Servis et");
+        }
+    }
+
+
+    class OcaktaCorba : Yemek
+    {
+        public virtual void Hazirla()
+        {
+            Console.WriteLine("Corba malzemelerini hazirla");
+        }
+
+        public virtual void Pisir()
+        {
+            Console.WriteLine("Ocakta Pisir");
+        }
+
+        public virtual void Bol()
+        {
+            Console.WriteLine("Kaselere koy");
+        }
+
+        public virtual void Servis()
+        {
+            Console.WriteLine("Servis et");
+        }
+    }
+
+
+
+
+
+
+
+}
